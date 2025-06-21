ac3c5f2c9b3af3b7f05609ce3225d71b15277dc0
Kanagat <sauranbaykanagat@gmail.com>
2025-06-21 19:41:12 +0500

    step ү

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const navLinks = document.querySelectorAll(".nav-link");
-const currentUrl = window.location.pathname;
+// const navLinks = document.querySelectorAll(".nav-link");
+// const currentUrl = window.location.pathname;
 
-navLinks.forEach(link => {
-    if (link.getAttribute("href") === currentUrl) {
-        link.classList.add("active");
-    }
+// navLinks.forEach(link => {
+//     if (link.getAttribute("href") === currentUrl) {
+//         link.classList.add("active");
+//     }
 
-    link.addEventListener("click", function() {
-        navLinks.forEach(el => el.classList.remove("active"));
-        this.classList.add("active");
-    });
-});
+//     link.addEventListener("click", function() {
+//         navLinks.forEach(el => el.classList.remove("active"));
+//         this.classList.add("active");
+//     });
+// });
